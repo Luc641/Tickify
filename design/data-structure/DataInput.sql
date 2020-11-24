1b172982d573c6392f8f0a394e4cae97aa91f966
LucLehmkuhl <l.lehmkuhl@student.fontys.nl>
2020-11-24 12:52:33 +0100

    Update DataInput.sql

--- design/data-structure/DataInput.sql
+++ design/data-structure/DataInput.sql
@@ -4,21 +4,21 @@ insert into administrator
 		('admin1@admin.com','@Administrator1');
 
 insert into customer
-values ('customer1@gmail.com', '@Customer1', true, 'john', 0031123456789, 'Venlo', '1950-01-20'),
-       ('customer2@hotmail.com', '@Customer2', false, 'andrew', 0041123456789, 'Spencer street 11 ', '2000-12-12'),
-       ('customer3@fontys.nl', '@Customer3', true, 'maria', 0049123456789, 'my house', '1994-04-27'),
-       ('customer4@fontys.nl', '@Customer4', true, 'mathew', 0034987654321, 'AMSTERDAM', '2002-08-20'),
-       ('customer5@fontys.nl', '@Customer5', true, 'john', 0049984635178, 'Barcelona', '2001-07-16'),
-       ('customer6@gmail.com', '@Customer6', true, 'tom', 0049985744365, 'Miami', '2001-06-15'),
-       ('customer7@web.de', '@Customer7', true, 'max', 0049094735292, 'New York', '1975-06-15'),
-       ('customer8@gmx.de', '@Customer8', false, 'carl', 0049947362354, 'Yorkshire', '1999-11-19'),
-       ('customer9@hotmail.com', '@Customer9', true, 'tim', 0049587858750, 'Cologne', '1997-12-12'),
-       ('customer10@outlook.com', '@Customer10', false, 'Lucas', 0049968652365, 'Berlin', '1998-09-29'),
-       ('customer11@pocketail.com', '@Customer11', true, 'Hans', 004954215478, 'Alicante', '1990-10-22'),
-       ('customer12@yahoo.com', '@Customer12', false, 'Günther', 0049256369856, 'Guatemala', '1996-09-14'),
-       ('customer13@yahoo.com', '@Customer13', true, 'Yannik', 004958748745, 'Nigeria', '1990-02-15'),
-       ('customer14@fontys.nl', '@Customer14', true, 'Jens', 0049587458742, 'Dubai', '1991-06-15'),
-       ('customer15@gmail.com', '@Customer15', true, 'Florian', 0049125412369, 'Japan', '1992-07-15');
+values ('customer1@gmail.com', '@Customer1', true, 'john', '0031123456789', 'Venlo', '1950-01-20'),
+       ('customer2@hotmail.com', '@Customer2', false, 'andrew', '0041123456789', 'Spencer street 11 ', '2000-12-12'),
+       ('customer3@fontys.nl', '@Customer3', true, 'maria', '0049123456789', 'my house', '1994-04-27'),
+       ('customer4@fontys.nl', '@Customer4', true, 'mathew', '0034987654321', 'AMSTERDAM', '2002-08-20'),
+       ('customer5@fontys.nl', '@Customer5', true, 'john', '0049984635178', 'Barcelona', '2001-07-16'),
+       ('customer6@gmail.com', '@Customer6', true, 'tom', '0049985744365', 'Miami', '2001-06-15'),
+       ('customer7@web.de', '@Customer7', true, 'max', '0049094735292', 'New York', '1975-06-15'),
+       ('customer8@gmx.de', '@Customer8', false, 'carl', '0049947362354', 'Yorkshire', '1999-11-19'),
+       ('customer9@hotmail.com', '@Customer9', true, 'tim', '0049587858750', 'Cologne', '1997-12-12'),
+       ('customer10@outlook.com', '@Customer10', false, 'Lucas', '0049968652365', 'Berlin', '1998-09-29'),
+       ('customer11@pocketail.com', '@Customer11', true, 'Hans', '004954215478', 'Alicante', '1990-10-22'),
+       ('customer12@yahoo.com', '@Customer12', false, 'Günther', '0049256369856', 'Guatemala', '1996-09-14'),
+       ('customer13@yahoo.com', '@Customer13', true, 'Yannik', '004958748745', 'Nigeria', '1990-02-15'),
+       ('customer14@fontys.nl', '@Customer14', true, 'Jens', '0049587458742', 'Dubai', '1991-06-15'),
+       ('customer15@gmail.com', '@Customer15', true, 'Florian', '0049125412369', 'Japan', '1992-07-15');
 
 insert into organizer
 	values
