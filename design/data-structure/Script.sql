35595294f7496de4d39f269f1228f040d6de260f
Joel Sebastian <j.sebastian@student.fontys.nl>
2020-11-24 12:21:27 +0100

    Update Script.sql

--- design/data-structure/Script.sql
+++ design/data-structure/Script.sql
@@ -1,19 +1,19 @@
 create table CUSTOMER (
 	C_Mail varchar(30) not null primary key,
-	C_PhoneNr varchar(15) not null,
 	C_Password varchar(30) not null,
-	Status varchar(30) not null default 'ACTIVE',
-	C_Address varchar(100) not null,
+	Status boolean not null default true,
 	C_Name varchar(50) not null,
+	C_PhoneNr int not null,
+	C_Address varchar(100) not null,
 	C_Birthdate date not null
 );
 
 create table ORGANIZER (
 	O_Mail varchar(30) not null primary key,
-	O_PhoneNr varchar(15) not null,
 	O_Password varchar(30) not null,
-	O_Address varchar(100) not null,
 	O_Name varchar(50) not null
+	O_PhoneNr int not null,
+	O_Address varchar(100) not null
 );
 
 create table ADMINISTRATOR (
@@ -24,7 +24,7 @@ create table ADMINISTRATOR (
 create table ORDERS (
 	OrderNumber int not null primary key,
 	OrderDate timestamp not null,
-	PaymentStatus varchar(10) default 'PENDING',
+	PaymentStatus boolean default false,
 	PaymentType varchar(30) default 'BANK TRANSFER',
 	CustomerMail varchar(30) not null,
 	foreign key (CustomerMail) references CUSTOMER(C_Mail)
@@ -32,9 +32,9 @@ create table ORDERS (
 
 create table EVENTS(
 	EventNr int not null primary key,
+	E_Name varchar (30) not null,
 	EventCategory varchar(30) not null,
 	Description varchar(6000),
-	E_Name varchar (30) not null,
 	E_Location varchar (150) not null,
 	E_Date timestamp not null,
 	OrganizerMail varchar(30) not null,
@@ -44,24 +44,23 @@ create table EVENTS(
 
 create table TICKET(
 	ID int not null primary key,
-	Price decimal(9,2) not null,
-	TicketCategory varchar(30) default 'A',
-	Order_Number int default null,
 	EventNumber int not null,
+	Price decimal(9,2) not null,
+	TicketCategory varchar(30) default null,
+	Order_Number int default null,
 	foreign key (EventNumber) references EVENTS(EventNr),
 	foreign key (Order_Number) references ORDERS(OrderNumber)
 );
 
 ALTER TABLE customer
 	ADD CONSTRAINT C_Email_Format CHECK (C_Mail LIKE '%@%.%'),
-	ADD CONSTRAINT C_PhoneNr_Format CHECK (C_PhoneNr LIKE '+%'),
+	ADD CONSTRAINT C_PhoneNr_Format CHECK (C_PhoneNr LIKE '00%'),
 	ADD CONSTRAINT C_Password_Format CHECK (LENGTH(C_Password) >= 8),
-	ADD CONSTRAINT Status_Values CHECK (Status IN ('ACTIVE', 'DISABLED')),
 	ADD CONSTRAINT C_Birthdate_check CHECK (C_Birthdate >= '1900-01-01' AND C_Birthdate < CURRENT_DATE);
 	
 ALTER TABLE organizer
 	ADD CONSTRAINT O_Email_Format CHECK (O_Mail LIKE '%@%.%'), 
-	ADD CONSTRAINT O_PhoneNr_Format CHECK (O_PhoneNr LIKE '+%'),
+	ADD CONSTRAINT O_PhoneNr_Format CHECK (O_PhoneNr LIKE '00%'),
 	ADD CONSTRAINT O_Password_Format CHECK (LENGTH(O_Password) >= 8);
 	
 ALTER TABLE administrator
@@ -71,11 +70,10 @@ ALTER TABLE administrator
 ALTER TABLE orders
 	add constraint OrderNumber_value check (OrderNumber >= 0),
 	ADD CONSTRAINT OrderDate_value CHECK (OrderDate <= CURRENT_TIMESTAMP),
-	ADD CONSTRAINT PaymentStatus_value CHECK (PaymentStatus IN ('PAID', 'PENDING', 'NOT PAID')),
 	ADD CONSTRAINT PaymentType_value CHECK (PaymentType IN ('BANK TRANSFER', 'PAYPAL', 'VISA', 'MAESTRO', 'AMERICAN EXPRESS', 'MASTERCARD'));
 	
 ALTER TABLE ticket
-	ADD CONSTRAINT Price_value CHECK (Price >= 0.00),
+	add constraint Price_value CHECK (Price >= 0.00),
 	add constraint ID_value check (ID >= 0);
 	
 ALTER TABLE events
