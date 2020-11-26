cd6b07074102f057273e2985fdc96697598f5f4b
MonikaDobreva <70884952+MonikaDobreva@users.noreply.github.com>
2020-11-26 15:58:44 +0100

    Update DataInput.sql

--- design/data-structure/DataInput.sql
+++ design/data-structure/DataInput.sql
@@ -36,8 +36,8 @@ insert into organizer
 insert into events
 	values
 		(1, 'The end of the world', 'CINEMA & THEATRE', 'New movie about the events leading to the end of the world', 'CineStar Metropolis', '2021-02-01 00:30:00', 'ticketing@outlook.com'),
-		(2, 'Halestorm again in Amsterdam!', 'CONCERTS AND FESTIVALS', 'Halestorm is coming to the Netherlands! The American rock band will be on stage at Ziggo Dome in Amsterdam in the winter of 2021. When we saw them for last time it was at a performance in Poppodium 013 Tilburg. The heavy rock of the American group showed to be not heavy enough. Nevertheless - or perhaps precisely because of this - the band is playing in an even bigger hall almost two years later. With little frills, a solid rock show will be given away in Ziggo Dome in Amsterdam in January 2021.', 'Ziggo Dome, Amsterdam', '2021-09-25 18:00:00', 'samoni@events.com'),
-		(3, 'Arsenal FC vs Tottenham Hotspur FC', 'SPORTS', 'Do not miss out on the North London derby - Arsenal FC vs Tottenham Hotspur FC - get your tickets to this classic football match here. By the way, the next clash available on stadium for the supporters will happen during the next season. From your point of view, who is going to beat the other? Will the Gunners take the lead this time? Or will the Spurs follow the same path? At 1st sight, this new season will be very exciting! The North London Derby will be on fire! You must not miss these epic clashes if you are a real fan!' , 'Emirates Stadium, London', '2021-09-31 20:45:00', 'sport2@yahoo.com'),
+		(2, 'Halestorm again in Amsterdam!', 'CONCERTS AND FESTIVALS', 'Halestorm is coming to the Netherlands! The American rock band will be on stage at Ziggo Dome in Amsterdam in the winter of 2021. When we saw them for last time it was at a performance in Poppodium 013 Tilburg. The heavy rock of the American group showed to be not heavy enough. Nevertheless - or perhaps precisely because of this - the band is playing in an even bigger hall almost two years later. With little frills, a solid rock show will be given away in Ziggo Dome in Amsterdam in January 2021.', 'Ziggo Dome, Amsterdam', '2021-01-31 20:45:00', 'samoni@events.com'),
+		(3, 'Arsenal FC vs Tottenham Hotspur FC', 'SPORTS', 'Do not miss out on the North London derby - Arsenal FC vs Tottenham Hotspur FC - get your tickets to this classic football match here. By the way, the next clash available on stadium for the supporters will happen during the next season. From your point of view, who is going to beat the other? Will the Gunners take the lead this time? Or will the Spurs follow the same path? At 1st sight, this new season will be very exciting! The North London Derby will be on fire! You must not miss these epic clashes if you are a real fan!' , 'Emirates Stadium, London', '2021-09-25 18:00:00', 'sport2@yahoo.com'),
 		(4, 'MONSTER TRUCKS in Rotterdam', 'FAMILY', 'Our MONSTER TRUCK events can give an adrenaline rush for the whole family. They are full of action and spectacular stunts that challenge gravity. World famous trucks participate in combination with our world-class drivers they push the insane Monster Trucks to the limit in freestyle and racing competitions. The whole family will be on the edge of their seats during this surprising, stunning and unforgettable event.', 'Stadion Feijenoord - De Kuip Rotterdam', '2021-04-10 13:00:00', 'monstert@hotmail.com'),
 		(5, 'Kwebbel & Sjnebbel', 'CINEMA AND THEATRE', null, 'Forum Theatre, Engelenkampstraat 3468, 6131ER Sittard', '2021-06-16 19:30:00', 'carlton@mail.com');
 
@@ -59,25 +59,10 @@ insert into orders
 		(14, '2020-01-05 23:59:59', true, 'BANK TRANSFER', 'customer13@yahoo.com'),
 		(15, '2020-01-31 18:36:42', false, 'BANK TRANSFER', 'customer8@gmx.de');
 
-INSERT INTO TICKET (ID, EventNumber, Price, TicketCategory, Order_Number) 
-    Values 
-        (101, 3, 59.0, 'c', 1)
-	    (102, 3, 35.0, 'a', 2)
-	    (103, 4, 26.0, 'c', 3)
-	    (104, 3, 48.0, 'b', 3)
-	    (105, 1, 48.0, 'd', 4)
-	    (106, 4, 28.0, 'c', 4)
-	    (107, 3, 35.0, 'a', 5)
-	    (108, 1, 71.0, 'c', 6)
-	    (109, 4, 32.0, 'c', 7)
-	    (110, 3, 26.0, 'd', 8)
-	    (111, 4, 51.0, 'b', 8)
-	    (112, 4, 51.0, 'a', 9)
-	    (113, 5, 80.0, 'b', 10)
-	    (114, 5, 63.0, 'a', 11)
-	    (115, 2, 73.0, 'c', 12)
-	    (116, 4, 26.0, 'b', 12)
-	    (117, 4, 70.0, 'b', 13)
-	    (118, 5, 80.0, 'a', 14)
-	    (119, 4, 28.0, 'a', 14)
-	    (120, 5, 22.0, 'd', 15);
+insert into ticket
+	values
+		(1, 1234.10, 'Group ticket', 1, 2),
+		(2, 10.25, 'Single ticket', null, 5),
+		(3, 9.99, null, 3, 1),
+		(4, 9.99, null, null, 1),
+		(5, 9.99, null, null, 1);
