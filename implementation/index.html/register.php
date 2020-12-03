94452629e2ac9ee52e1d9d9eaadfca499959e099
MiroMiloloza <70884219+MiroMiloloza@users.noreply.github.com>
2020-12-03 16:44:59 +0100

    DBS Connection

--- implementation/index.html/register.php
+++ implementation/index.html/register.php
@@ -1,5 +1,8 @@
 <!DOCTYPE html>
 
+<?php include 'databaseConnection.php'?>
+
+
    <html> 
         
         
@@ -15,36 +18,34 @@
             
             <hr>
             
-            <label for="uname"> <b> Username: </b></label>
-            <input type="text" placeholder="Enter Username" name="uname" id="uname" required>
-            
-            <label for="fname"> <b> Username: </b></label>
-            <input type="text" placeholder="Enter Username" name="uname" id="uname" required>
-            
             <label for="email"> <b> E-Mail: </b></label>
             <input type="text" placeholder="Enter E-Mail Address" name="email" id ="email" required>
+
+            <label for="psw"> <b> Password: </b></label>
+            <input type="password" placeholder="Enter Password" name="psw" id="psw" required>
+
+            <label for="psw-repeat"> <b> Repeat Password: </b></label>
+            <input type="password" placeholder="Repeat Password" name="psw-repeat" id="psw-repeat" required>
+
+            <label for="uname"> <b> Username: </b></label>
+            <input type="text" placeholder="Enter Username" name="uname" id="uname" required>
+
+            <label for="phone"> <b> Phone Number: </b></label>
+            <input type="text" placeholder="Enter Phone Number" name="phone" id="phone" required>
+
+            <label for="address"> <b> Address: </b></label>
+            <input type="text" placeholder="Enter Your Adress" name="address" id="address" required>
             
             <label for="bdate"> <b> Birth Date: </b></label>
             <input type="date" placeholder="Enter Date of Birth" name="bdate" id ="bdate" required>
             
-            <label for="psw"> <b> Password: </b></label>
-            <input type="password" placeholder="Enter Password" name="psw" id="psw" required>
             
-            <label for="psw-repeat"> <b> Repeat Password: </b></label>
-            <input type="password" placeholder="Repeat Password" name="psw-repeat" id="psw-repeat" required>
             
             <button type="submit" class="registerbtn"> Confirm </button>
             <button type="button" class="registerbtn"> Cancel </button>
             
             
-            </div>
             
-            <div class="container signin">
-            
-            <label for="loginbtn"> <a> Already have an account? </a></label>
-            <button type="button" class="loginbtn"> Log-in </button>
-                
-            </div>
             
             
         
