29ee098cc5aae2bf196236cb381bd6a611c17ae0
Joel Sebastian <j.sebastian@student.fontys.nl>
2020-12-09 20:09:38 +0100

    Update navigation.php
    Now Log in is working without security

--- implementation/index.html/navigation.php
+++ implementation/index.html/navigation.php
@@ -1,9 +1,7 @@
 <!DOCTYPE html>
-<!--
-To change this license header, choose License Headers in Project Properties.
-To change this template file, choose Tools | Templates
-and open the template in the editor.
--->
+
+<?php include 'databaseConnection.php';?>
+
 <html>
     <head>
         <title>Tickify - Navigation Bar</title>
@@ -23,13 +21,19 @@ and open the template in the editor.
                     <button class="dropdownbtn">Categories</button>
 
                     <div class="dropdown-content">
+                        
+                        <?php
 
-                        <a href="">Cinema & Theatre</a>
-                        <a href="">Musicals/Festivals/Concerts & Shows</a>
-                        <a href="">Sports</a>
-                        <a href="">Family</a>
-                        <a href="">Expositions</a>
+                            $categories = array("Cinema & Theatre", "Musicals/Festivals/Concerts & Shows", "Sports", "Family", "Expositions");
+                            
+                            foreach ($categories as $category){
 
+                        ?>
+
+                        <a href=""><?php echo $category; ?></a>
+                        
+                            <?php } ?>
+                        
                     </div>
                 
                 </div>
@@ -45,33 +49,164 @@ and open the template in the editor.
 
                     <div class="profileOptions">
 
-                        <form>
+                        <?php
+    
+                            $link = $_SERVER['REQUEST_URI']; //get the link of actual page
 
-                            <fieldset>
+                            $displayForm = true;
 
-                                <legend>Log in details </legend>
+                            if (isset($_POST["loginBtn"])){ //trying to log in
 
-                                <p>
-                                    <label for="user">Username:</label>
-                                    <input type="text" id="user" name="user"><br><br>
+                                //Get values from the form
+                                $user = $_POST["user"];
+                                $password = $_POST["password"];
 
-                                </p>
+                                try { //Check if user entered is in the system
+                                    $stmt = $conn->prepare("SELECT C_Password "
+                                                            . "FROM customer "
+                                                            . "WHERE C_Mail = ?");
 
-                                <p>
-                                    <label for="password">Password:</label>
-                                    <input type="password" id="password" name="password"><br><br>
-                                </p>
+                                    //Execute the previous defined statement
+                                    $stmt->execute([$user]); // table for C_mail = $user
 
-                                <button id="logIn">Log In</button>
+                                    //
+                                    $result = $stmt->setFetchMode(PDO::FETCH_NUM);
 
-                                <button id="register">Register</button>
+                                    $row = $stmt->fetch();
 
-                            </fieldset>
+                                    $customerPass = $row[0]; //password for customer table
 
-                        </form>
+                                    $stmt = $conn->prepare("SELECT O_Password "
+                                                            . "FROM organizer "
+                                                            . "WHERE O_Mail = ?");
+
+                                    //Execute the previous defined statement
+                                    $stmt->execute([$user]); // table for O_mail = $user
+
+                                    //
+                                    $result = $stmt->setFetchMode(PDO::FETCH_NUM);
+
+                                    $row = $stmt->fetch();
+
+                                    $organizerPass = $row[0]; //password for organizer table
+
+                                    $stmt = $conn->prepare("SELECT A_Password "
+                                                            . "FROM administrator "
+                                                            . "WHERE A_Mail = ?");
+
+                                    //Execute the previous defined statement
+                                    $stmt->execute([$user]); // table for A_mail = $user
+
+                                    //
+                                    $result = $stmt->setFetchMode(PDO::FETCH_NUM);
+
+                                    $row = $stmt->fetch();
+
+                                    $adminPass = $row[0]; //password for admin table
+
+                                } catch (PDOException $e){ //report error as user not in system
+                                    echo "Invalid username or password";    
+                                }
+
+                                if (isset($customerPass)){
+                                    if ($customerPass == $password){ //log in if user is a customer and correct password
+                                        $displayForm = false;
+                                    }
+
+                                    else {
+                                        echo "Invalid username or password";
+                                    }
+
+                                }
+
+                                else if (isset($organizerPass)){
+                                    if ($organizerPass == $password){ //log in if user is an organizer and correct password
+                                        $displayForm = false;
+                                    }
+
+                                    else {
+                                        echo "Invalid username or password";
+                                    }
+                                }
+
+                                else if (isset($adminPass)){ //log in if user is an admin and correct password
+                                    if ($adminPass == $password){
+                                        $displayForm = false;
+                                    }
+
+                                    else {
+                                        echo "Invalid username or password";
+                                    }
+                                }
+
+                                else {
+                                    echo "Invalid username or password";
+                                }
+
+                                //Erase all the initialized variables
+                                unset($_POST["loginBtn"]);
+                                unset($user);
+                                unset($password);
+                                unset($customerPass);
+                                unset($organizerPass);
+                                unset($adminPass);
+                            }
+
+                            if (isset($_POST["logoutBtn"])){ //log out case
+                                $displayForm = true;
+                                unset($_POST["logoutBtn"]);
+                            }
+
+                            if ($displayForm){ // Show log in form
+
+                        ?>
+
+                            <form name="login" method="post" action=<?php $link ?>>
+
+                                <fieldset>
+
+                                    <legend>Log in details </legend>
+
+                                    <p>
+                                        <label for="user">Username:</label>
+                                        <input type="text" id="user" name="user"><br><br>
+
+                                    </p>
+
+                                    <p>
+                                        <label for="password">Password:</label>
+                                        <input type="password" id="password" name="password"><br><br>
+                                    </p>
+
+                                    <input type="submit" name="loginBtn" id="logIn" value="Log In">
+
+                                </fieldset>
+
+                            </form>
+
+                        <?php
+
+                            } else { // Show log out form/button
+
+                        ?>
+
+                            <form name="logout" method="post" action=<?php $link ?>>
+
+                                <fieldset>
+
+                                    <input type="submit" name="logoutBtn" id="logIn" value="Log Out">
+
+                                </fieldset>
+
+                            </form>
+
+                        <?php
+
+                            }
+
+                        ?>
 
                         <a href="">My profile</a>
-                        <a href="">Log out</a>
 
                     </div>
                 </div>
